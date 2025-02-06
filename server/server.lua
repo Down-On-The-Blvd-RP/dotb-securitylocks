6aa0fea724dde86a591d5c23999d44a8a69b8f4e
Claiborne <84554614+ckaysone380@users.noreply.github.com>
2025-02-05 21:59:00 -0500

    Add files via upload
    Added in version notification in server lua
    
    Signed-off-by: Claiborne <84554614+ckaysone380@users.noreply.github.com>

--- server/server.lua
+++ server/server.lua
@@ -1,4 +1,7 @@
 local isLocked = false
+local currentVersion = "1.0"
+local latestVersion = "1.0" -- This will be updated based on a real check later (for now, assume it's the same version)
+local versionCheckUrl = "https://raw.githubusercontent.com/YourGitHubRepo/dotb-securitylocks/main/version.txt" -- Replace with the URL where the version is hosted
 
 -- Function to check if the time is within locked hours
 local function IsStoreClosed()
@@ -53,4 +56,47 @@ Citizen.CreateThread(function()
             SendRobberyNotification(Config.RobberyNotification.jobType)
         end
     end
+end)
+
+-- Function to display the current version in the TXAdmin console
+Citizen.CreateThread(function()
+    print("==============================================")
+    print("dotb-securitylocks - Current Version: " .. currentVersion)
+    print("==============================================")
+
+    -- Check if a newer version is available
+    PerformVersionCheck()
+end)
+
+-- Function to check for a newer version of dotb-securitylocks
+function PerformVersionCheck()
+    PerformHttpRequest(versionCheckUrl, function (statusCode, responseText, headers)
+        if statusCode == 200 then
+            local latestVersionFromRepo = responseText:match("(%d+%.%d+)") -- Assuming the version format is "X.X"
+
+            if latestVersionFromRepo and latestVersionFromRepo ~= currentVersion then
+                print("WARNING: You are using an outdated version of dotb-securitylocks!")
+                print("Current Version: " .. currentVersion)
+                print("Latest Version: " .. latestVersionFromRepo)
+                print("Please update to the latest version for bug fixes and new features.")
+            else
+                print("dotb-securitylocks is up to date (Version " .. currentVersion .. ").")
+            end
+        else
+            print("Error checking for updates. Status Code: " .. statusCode)
+        end
+    end, "GET")
+end
+
+-- Function to notify admin when version is outdated
+AddEventHandler('dotb-securitylocks:notifyOutdatedVersion', function()
+    print("==============================================")
+    print("WARNING: Your version of dotb-securitylocks is outdated!")
+    print("Please update to the latest version to avoid any issues.")
+    print("==============================================")
+end)
+
+-- Add event handler for potential version check or other logic
+AddEventHandler('dotb-securitylocks:checkVersion', function()
+    PerformVersionCheck()
 end)
